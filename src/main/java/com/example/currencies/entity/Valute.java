07cae0a5884b8f1ab7c287f3f7e54e40a9f5c728
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-10-10 01:19:13 +0500

    Добавил entity для работы с ЦБ

---
+++ src/main/java/com/example/currencies/entity/Valute.java
@@ -0,0 +1,37 @@
+package com.example.currencies.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Valute {
+
+    @JacksonXmlProperty(localName = "ID", isAttribute = true)
+    private String id;
+
+    @JacksonXmlProperty(localName = "NumCode")
+    private Integer numCode;
+
+    @JacksonXmlProperty(localName = "CharCode")
+    private String charCode;
+
+    @JacksonXmlProperty(localName = "Nominal")
+    private Integer nominal;
+
+    @JacksonXmlProperty(localName = "Name")
+    private String name;
+
+    @JacksonXmlProperty(localName = "Value")
+    private BigDecimal value;
+
+    @JacksonXmlProperty(localName = "VunitRate")
+    private BigDecimal vunitRate;
+}
