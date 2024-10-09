8d58aa14734f61828d3e64e577ae14562929bbd0
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-10-10 01:50:57 +0500

    Добаивл кастомные исключения

---
+++ src/main/java/com/example/currencies/exception/CurrencyNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.currencies.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CurrencyNotFoundException extends RuntimeException {
+
+    private final String code;
+
+    public CurrencyNotFoundException(String code) {
+        super("currency not found");
+        this.code = code;
+    }
+}
