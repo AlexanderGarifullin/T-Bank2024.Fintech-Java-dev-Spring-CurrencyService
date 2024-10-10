8d623ce2bd30741af7b59f013c5aefe12610f7b1
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-10-10 17:51:12 +0500

    Добавил профиль для приложения

--- src/main/java/com/example/currencies/services/DataLoader.java
+++ src/main/java/com/example/currencies/services/DataLoader.java
@@ -3,11 +3,13 @@ package com.example.currencies.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("prod")
 public class DataLoader {
 
     private static final Logger logger = LoggerFactory.getLogger(DataLoader.class);
